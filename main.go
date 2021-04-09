eb475b0c1dd33d58fb37980f6ee3e0b998451c34
Dennis Mubamba <dennism501@outlook.com>
2021-04-10 01:21:05 +0200

    :construction: Getting Todos from static file

--- main.go
+++ main.go
@@ -1,18 +1,40 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 )
 
-type testHandler struct {
-	Message string
+type Todo struct {
+	ID        int    `json:"id"`
+	Message   string `json:"message"`
+	CreatedAt string `json:"createdAt"`
 }
 
-func (f *testHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(f.Message))
+var todoList []Todo
+
+func init() {
+	todoJSON := `[{"id":1,"message":"Create Todo list app with GoLang", "createdAt": "2015-12-12"}]`
+	err := json.Unmarshal([]byte(todoJSON), &todoList)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func todoHandler(w http.ResponseWriter, r *http.Request) {
+
+	todoListJson, err := json.Marshal(todoList)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(todoListJson)
 }
 
 func main() {
-	http.Handle("/test", &testHandler{Message: "Hello World!!!"})
+
+	http.HandleFunc("/todos", todoHandler)
 	http.ListenAndServe(":5000", nil)
 }
